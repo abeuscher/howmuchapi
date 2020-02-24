98e5b2a782c7e03e9e4bef3b56d0cbb98a259863
Alex Beuscher <alexbeuscher@gmail.com>
2020-02-24 02:19:42 -0800

    Server responding with data from mongo

--- appserver/dist/index.js
+++ appserver/dist/index.js
@@ -5,48 +5,47 @@ const ObjectId = require("mongodb").ObjectID;
 const fs = require("file-system");
 const cors = require("cors");
 
-const CONNECTION_URL = "mongodb://mongodb/test";
+const CONNECTION_URL = "mongodb://mongodb/";
 const DATABASE_NAME = "local";
 
 var app = Express();
 app.use(cors());
 app.use(BodyParser.json());
 app.use(BodyParser.urlencoded({ extended: true }));
-var database, collection;
-var corsOptions = {
+
+let corsOptions = {
     origin: 'http://localhost',
     optionsSuccessStatus: 200
 }
 app.listen(5000, () => {
+
+    app.get('/', cors(corsOptions), (request, response) => {
+
+        readDB(response, request);
+
+    })
+
+});
+
+function readDB(res, req) {
     MongoClient.connect(CONNECTION_URL, { useNewUrlParser: true }, (error, client) => {
+
         if (error) {
             throw error;
         }
         database = client.db(DATABASE_NAME);
         collection = database.collection("testcollection");
-        var cursor = collection.find();
-        cursor.each(function (err, doc) {
-            if (doc != null)
-                console.log(doc.name);
+
+        collection.find({}).toArray((error, result) => {
+            if(error) {
+                return res.status(500).send(error);
+            }
+            res.send(result);
         });
-        console.log("Connected to `" + DATABASE_NAME + "`!");
+
+
     });
-    /*
-    app.get('/', cors(corsOptions), (request, response) => {
-
-            sendFileContent(response, "index.html", "text/html");
-
-      })
-      */
-    app.get('/', cors(corsOptions), (request, response) => {
-
-        response.writeHead(200, { 'Content-Type': 'text/plain' });
-
-        response.end('Hello World\n');
-
-    })
-
-});
+}
 
 function sendFileContent(response, fileName, contentType) {
     fs.readFile(fileName, function (err, data) {
